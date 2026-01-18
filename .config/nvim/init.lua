0867fc5a4c7ad994900e898e9256c8a187bd1be6
alifoo <alissonayres.trb@gmail.com>
2026-01-17 23:46:50 -0300

    fix: removed pigment local files and modified i3 config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1230,6 +1230,10 @@ require("lazy").setup({
 		lazy = false,
 		priority = 1000,
 		config = function()
+			-- Optional: Setup to choose a theme (vintage, dark, or canvas)
+			require("pigment.init").setup({ theme = "canvas" })
+
+			-- Load the colorscheme
 			require("pigment.init").colorscheme()
 		end,
 	},
@@ -1374,7 +1378,7 @@ require("lazy").setup({
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
+		main = "nvim-treesitter.config", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
 			ensure_installed = {
